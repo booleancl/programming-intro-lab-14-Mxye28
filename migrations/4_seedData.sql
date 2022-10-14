e92dad77024a448b4f2789035a75265c630e618a
Maryelin <mayeat94@gmail.com>
2022-10-14 11:06:18 -0300

    basededatosmigrations

--- migrations/4_seedData.sql
+++ migrations/4_seedData.sql
@@ -1,1 +1,5 @@
+\c blog
 
+\COPY users (email) FROM 'C:\Users\mxye2\intro_dev\programming-intro-lab-14-Mxye28\data\users.csv' csv HEADER;
+\COPY posts FROM 'C:\Users\mxye2\intro_dev\programming-intro-lab-14-Mxye28\data\posts.csv' csv HEADER;
+\COPY FROM 'C:\Users\mxye2\intro_dev\programming-intro-lab-14-Mxye28\data\comentarios.csv' csv HEADER;
